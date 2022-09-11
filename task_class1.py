7edf80f359b229dc8b9fdad2fd81ec531a35c002
adash <7Xc6CQPY3G>
2022-09-11 17:33:39 +0300

    ver.3 MY ferm

--- task_class1.py
+++ task_class1.py
@@ -1,5 +1,7 @@
 # Моя зверюшка
 # Виртуальный питомец, о котором пользователь может позаботиться
+import random
+
 
 class Critter(object):
     """Виртуальный питомец"""
@@ -53,17 +55,21 @@ class Critter(object):
 
 
 def main():
-    crit_name = input("Как назовете свою зверюшку? ")
-    crit = Critter(crit_name)
+    crit_name1 = input("Как назовете свою 1 зверюшку? ")
+    crit_name2 = input("Как назовете свою 2 зверюшку? ")
+    crit_name3 = input("Как назовете свою 3 зверюшку? ")
+    crit1 = Critter(crit_name1, random.randint(0, 10), random.randint(0, 10))
+    crit2 = Critter(crit_name2, random.randint(0, 10), random.randint(0, 10))
+    crit3 = Critter(crit_name3, random.randint(0, 10), random.randint(0, 10))
 
     choice = None
     while choice != "0":
         print(
-            """Моя зверюшка
+            """Мои зверюшки
             0 - Выйти
             1 - Узнать о самочуствии
-            2 - Покормить зверюшку
-            3 - Поиграть со зверюшку
+            2 - Покормить зверюшек
+            3 - Поиграть со зверюшек
             4 - Посказка
             """)
         choice = input("Ваш выбор: ")
@@ -71,15 +77,21 @@ def main():
         if choice == "0":
             print("Досвидания")
         elif choice == "1":
-            crit.talk()
+            crit1.talk()
+            crit2.talk()
+            crit3.talk()
         elif choice == "2":
             eat = int(input("Введите сколько корма хотите дать: "))
-            crit.eat(eat)
+            crit1.eat(eat)
+            crit2.eat(eat)
+            crit3.eat(eat)
         elif choice == "3":
             fun = int(input("Введите сколько времени хотит потратить на игру: "))
-            crit.play(fun)
+            crit1.play(fun)
+            crit2.play(fun)
+            crit3.play(fun)
         elif choice == "4":
-            print(crit)
+            print(crit1)
         else:
             print(f"В меню нету такого выбора {choice}")
 
